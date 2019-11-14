2912d1b4395b3e2ad7c16e46f9eca7a6f91301a4
Makeven Yan <makevenyan@Makevens-MacBook-Pro.local>
2019-11-13 22:18:30 -0800

    create api helpers for front end

---
+++ front-end/src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Cards from '../views/Cards.vue';
+import New from '../views/New.vue';
+import Show from '../views/Show.vue';
+import Edit from '../views/Edit.vue';
+import Test from '../views/Test.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  linkActiveClass: 'active',
+  routes: [
+    {
+      path: '/',
+      redirect: '/cards'
+    },
+    {
+      path: '/cards',
+      name: 'cards',
+      component: Cards
+    },
+    {
+      path: '/cards/new',
+      name: 'new-card',
+      component: New
+    },
+    {
+      path: '/cards/:id',
+      name: 'show',
+      component: Show
+    },
+    {
+      path: '/cards/:id/edit',
+      name: 'edit',
+      component: Edit
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: Test
+    }
+  ]
+});
