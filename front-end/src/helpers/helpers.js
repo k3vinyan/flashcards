d3ee14fdcaa04d2a46ca8d9d5a908fa77b5b9da8
Makeven Yan <makevenyan@Makevens-MacBook-Pro.local>
2019-11-15 18:10:53 -0800

    fix error handler

--- front-end/src/helpers/helpers.js
+++ front-end/src/helpers/helpers.js
@@ -2,31 +2,45 @@ import axios from 'axios';
 
 const baseURL = 'http://localhost:3000/cards/';
 
-const handleError = fn => (...params) => {
-    fn(...params).catch(err => {
-        console.log(err);
-    })
-}
-
 export const api = {
-    getCard: handleError(async id => {
-        const res = await axios.get(baseURL + id);
-        return res.data;
-    }),
-    getCards: handleError(async () => {
-        const res = await axios.get(baseURL);
-        return res.data;
-    }),
-    deleteCard: handleError(async id => {
-        const res = await axios.delete(baseURL + id);
-        return res.data;
-    }),
-    createCard: handleError(async payload => {
-        const res = await axios.post(baseURL, payload);
-        return res.data;
-    }),
-    updateCard: handleError(async payload => {
-        const res = await axios.put(baseURL + payload._id, payload);
-        return res.data;
-    })
+    getCard: async id => {
+        try {
+            const res = await axios.get(baseURL + id);
+            return res.data;
+        } catch(e) {
+            console.error(e);
+        }
+    },
+    getCards: async () => {
+        try {
+            const res = await axios.get(baseURL);
+            return res.data;
+        } catch(e) {
+            console.error(e);
+        }
+    },
+    createCard: async payload => {
+        try {
+            const res = await axios.post(baseURL, payload);
+            return res.data;
+        } catch(e) {
+            console.error(e);
+        }
+    },
+    updateCard: async payload => {
+        try {
+            const res = await axios.post(baseURL + payload._id, payload);
+            return res.data;
+        } catch(e) {
+            console.error(e);
+        }
+    },
+    deleteCard: async id => {
+        try {
+            const res = await axios.delete(baseURL + id);
+            return res.data;
+        } catch(e) {
+            console.error(e);
+        }
+    }
 }
