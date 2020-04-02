5116132d2a07d2a0e6e2e94687c812e0fd2783a9
Makeven Yan <makevenyan@Makevens-MacBook-Pro.local>
2020-04-01 17:12:01 -0700

    create server

---
+++ fserver.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+
+app = express();
+
+app.use(serveStatic(__dirname + "/front-end/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log(__dirname + "front-end/dist");
+console.log('server started '+ port);
