7d548a9b087d8d0e39b9e7699844ae0441222fad
Makeven Yan <makevenyan@Makevens-MacBook-Pro.local>
2020-04-02 14:51:48 -0700

    update script for deploy

--- fserver.js
+++ fserver.js
@@ -7,5 +7,5 @@ app = express();
 app.use(serveStatic(__dirname + "/front-end/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
-console.log(__dirname + "front-end/dist");
+console.log(__dirname + "/front-end/dist");
 console.log('server started '+ port);
